e7175bfe649072d4f7e233142b23c3be1bf38a62
Kurup <160393536+kurupdevs@users.noreply.github.com>
2026-08-11 19:24:37 +0530

    style: min

--- modules/ping.py
+++ modules/ping.py
@@ -1,12 +1,7 @@
-import asyncio
-from pyrogram import Client,filters
-
 async def setup(c):
+ from pyrogram import filters
  c.on_message(filters.command("ping",prefixes=".")&filters.me)(p)
 
 async def p(c,m):
- import time
- s=time.time()
- msg=await m.edit("Pong!")
- e=time.time()
- await msg.edit(f"**Pong!** `{round((e-s)*1000)}ms`")
+ import time;s=time.time();msg=await m.edit("Pong!");e=time.time()
+ await msg.edit(f"Pong! `{round((e-s)*1000)}ms`")
