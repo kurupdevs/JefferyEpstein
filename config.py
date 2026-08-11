966663e0ac015f1e45cc046518ce8f6d24cfac24
Kurup <160393536+kurupdevs@users.noreply.github.com>
2026-08-11 19:06:57 +0530

    fix: correct import order

--- config.py
+++ config.py
@@ -1,11 +1,4 @@
-# Config Module for JEK Userbot
 import os
-from environs import Env
-
-env = Env()
-env.read_env()
-
-API_ID = env.int("API_ID", 0)  # type: int
-API_HASH = env.str("API_HASH", "")  # type: str
-PREFIX = env.str("PREFIX", ".")  # type: str
-LOG_CHANNEL = env.int("LOG_CHANNEL", 0)  # Validate
+API_ID=int(os.getenv("API_ID",0))
+API_HASH=os.getenv("API_HASH","")
+STRING_SESSION=os.getenv("STRING_SESSION","")
