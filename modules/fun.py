1f6b8febaa1ee73ea32aca025c30b68746642243
Kurup <160393536+kurupdevs@users.noreply.github.com>
2026-08-11 19:21:21 +0530

    style: align code

--- modules/fun.py
+++ modules/fun.py
@@ -1,9 +1,13 @@
-# Fun module
-from pyrogram import Client, filters
-from pyrogram.types import Message
+"""Fun module."""
 import random
+from pyrogram import Client,filters
 
-@Client.on_message(filters.command("joke"))
-async def joke_handler(client: Client, message: Message):
-    jokes = ["Why did the bot cross the road?", "Knock knock!"]
-    await message.reply(random.choice(jokes))
+FACTS=["Honey never spoils.","A day on Venus>year on Venus.","Bananas are berries."]
+JOKES=["Why atoms? They make up everything!","Fake spaghetti? Impasta!"]
+
+async def setup(c):
+ c.on_message(filters.command("fact",prefixes=".")&filters.me)(f)
+ c.on_message(filters.command("joke",prefixes=".")&filters.me)(j)
+
+async def f(c,m):await m.edit(f"**Fact:** {random.choice(FACTS)}")
+async def j(c,m):await m.edit(f"**Joke:** {random.choice(JOKES)}")
--- modules/help.py
+++ modules/help.py
@@ -1,6 +1,15 @@
+"""Help module."""
 from pyrogram import Client,filters
 
-HELP=""".afk .alive .ping .spam .fact .joke .ban .mute .help"""
+HELP="""**Commands:**
+• .afk - AFK
+• .alive - Uptime
+• .ping - Latency
+• .spam - Spam
+• .fact - Facts
+• .joke - Jokes
+• .ban/.mute - Manage
+• .help - Menu"""
 
 async def setup(c):c.on_message(filters.command("help",prefixes=".")&filters.me)(h)
-async def h(c,m):await m.edit(f"**Commands:** {HELP}")
+async def h(c,m):await m.edit(HELP)
