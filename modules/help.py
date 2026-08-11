1d85d81b8fb14b6cfd46466411d0579dfa14eb45
Kurup <160393536+kurupdevs@users.noreply.github.com>
2026-08-11 19:09:58 +0530

    refactor: rename internal variables

--- modules/help.py
+++ modules/help.py
@@ -1,20 +1,7 @@
+# Help module for command listing
 from pyrogram import Client, filters
 from pyrogram.types import Message
 
-HELP_TEXT = """
-**Available Commands:**
-
-• `.afk` - Set AFK status
-• `.alive` - Check bot uptime
-• `.ping` - Check latency
-• `.spam` - Spam messages
-• `.help` - Show this menu
-"""
-
-
-async def setup(client: Client):
-    client.on_message(filters.command("help", prefixes=".") & filters.me)(help_handler)
-
-
+@Client.on_message(filters.command("help"))
 async def help_handler(client: Client, message: Message):
-    await message.edit(HELP_TEXT)
+    await message.reply("Commands: /ping, /afk, /alive, /help, /spam")
