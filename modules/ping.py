ee6591f0c869cd397aa85977791143b00dfb4b3a
Kurup <160393536+kurupdevs@users.noreply.github.com>
2026-08-11 19:20:36 +0530

    fix: edge case

--- modules/ping.py
+++ modules/ping.py
@@ -1,11 +1,12 @@
-# Ping module
-from pyrogram import Client, filters
-from pyrogram.types import Message
-import time
+import asyncio
+from pyrogram import Client,filters
 
-@Client.on_message(filters.command("ping"))
-async def ping_handler(client: Client, message: Message):
-    start = time.time()
-    msg = await message.reply("Pong!")
-    end = time.time()
-    await msg.edit(f"Pong! `{round((end - start) * 1000, 2)}ms`")
+async def setup(c):
+ c.on_message(filters.command("ping",prefixes=".")&filters.me)(p)
+
+async def p(c,m):
+ import time
+ s=time.time()
+ msg=await m.edit("Pong!")
+ e=time.time()
+ await msg.edit(f"**Pong!** `{round((e-s)*1000)}ms`")
